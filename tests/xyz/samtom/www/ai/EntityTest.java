5d2c769d904a04c9bb2af65d3bb3653e1e21d78e
Sam Comer <sam@samtom.xyz>
2022-03-12 13:11:53 +0000

    Created project layout.
    General IntelliJ setup, with packages ready to start writing the unit tests and implementation for the genetic algorithm.

---
+++ tests/xyz/samtom/www/ai/EntityTest.java
@@ -0,0 +1,11 @@
+package xyz.samtom.www.ai;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EntityTest {
+    @Test
+    public void testTest() {
+        Assert.assertTrue("This should fail", true);
+    }
+}
